79518e2027af01b3c1f248d946a18dd4799fdc4b
Axel Gromat <axel.gromat@gmail.com>
2022-05-11 18:50:56 +0200

    Fixing config for tree builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('galilee_import_export');
+        $treeBuilder = new TreeBuilder('galilee_import_export');
+        // $rootNode = $treeBuilder->root('galilee_import_export');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
